fb6ba0e3567a3695ebf6fc9f919c5ecedf8fa3be
Phil St Croix <pstcroix1971@gmail.com>
2023-10-24 11:25:25 -0230

    copied peters starter files in

--- server.js
+++ server.js
@@ -1,32 +1,82 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const crypto = require('crypto');
+global.DEBUG = false;
+const http = require('http');
+const fs = require('fs');
+const { parse } = require('querystring');
+const { newToken, tokenCount } = require('./token');
 
-const app = express();
-const PORT = 3000;
+const server = http.createServer(async (req, res) => {
+    let path = './public/';
+    switch(req.url) {
+        case '/':
+            path += "index.html";
+            res.statusCode = 200;
+            fetchFile(path);
+            break;
+        case '/new':
+            if (req.method === 'POST') {
+                collectRequestData(req, result => {
+                    var theToken = newToken(result.username);
+                    res.write(`
+                    <!doctype html>
+                    <html>
+                    <body>
+                        ${result.username} token is ${theToken} <br />
+                        <a href="http://localhost:3000">[home]</a>
+                    </body>
+                    </html>
+                `);
+                res.end();
+                });
+            } 
+            else {
+                path += "newtoken.html";
+                res.statusCode = 200;
+                fetchFile(path);
+            };
+            break;
+        case '/count':
+            var theCount = await tokenCount();
+            res.end(`
+                <!doctype html>
+                <html>
+                <body>
+                    Token count is ${theCount} <br />
+                    <a href="http://localhost:3000">[home]</a>
+                </body>
+                </html>
+            `);
+        default:
+            break;
+    };
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  // Render your HTML form here
-  res.sendFile(__dirname + '/public/form.html');
+    function fetchFile(path) {
+        fs.readFile(path, function(err, data) {
+            if(err) {
+                console.log(err);
+                res.end();
+            } else {
+                if(DEBUG) console.log('file was served.');
+                res.writeHead(res.statusCode, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
+            }   
+        })
+    };
 });
+server.listen(3000);
 
-app.post('/generate-token', (req, res) => {
-  const username = req.body.username;
-  const token = generateToken(username);
-  res.send(`Your generated token is: ${token}`);
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-function generateToken(username) {
-  // Generate a random buffer of 16 bytes (128 bits)
-  const randomBytes = crypto.randomBytes(16);
-  // Convert the random buffer to a hexadecimal string
-  const token = randomBytes.toString('hex');
-  return token;
+function collectRequestData(request, callback) {
+    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if(request.headers['content-type'] === FORM_URLENCODED) {
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            callback(parse(body));
+        });
+    }
+    else {
+        callback(null);
+    }
 }
